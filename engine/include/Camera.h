c9cf309d391d1d808fb6805f3736bacadb8115fc
Matteo CAVAGNINO <matteo.cavagnino@acteongroup.com>
2025-01-21 12:16:35 +0100

    constexpr optimization for size ration comp.

--- engine/include/Camera.h
+++ engine/include/Camera.h
@@ -10,7 +10,8 @@ namespace shak
     {
 
     public:
-        Camera(sf::FloatRect viewport, std::shared_ptr<sf::VertexArray> va = nullptr, std::shared_ptr<sf::Texture> texture = nullptr) : GameObject(va, texture), m_view{ std::make_shared<sf::View>(viewport) }
+        Camera(sf::FloatRect viewport, std::shared_ptr<sf::VertexArray> va = nullptr, std::shared_ptr<sf::Texture> texture = nullptr)
+            : GameObject(va, texture), m_bg{ nullptr }, m_view{ std::make_shared<sf::View>(viewport) }
         {
         }
         ~Camera() { std::cout << "destroyed camera" << std::endl; }
@@ -33,14 +34,21 @@ namespace shak
 
         inline void SetBackground(const std::shared_ptr<Background>& bg) { m_bg = bg; }
 
+        // Compute size ratio at compile time
+        inline constexpr void SetBackgroundSize(const sf::Vector2f& size) { m_bgSize = sf::Vector2f{ 1.f, 1.f }.componentWiseDiv(size); }
+
         void Update(float dt) override
         {
             GameObject::Update(dt);
             if (m_bg)
             {
-                sf::Vector2f camPos = { m_view->getCenter().x - m_view->getSize().x / 2, m_view->getCenter().y - m_view->getSize().y / 2 };
-                auto camSize = m_view->getSize();
-                m_bg->SetScreenCoords({ camPos, camSize });
+                const sf::Vector2f camPos{ m_view->getCenter().x - m_view->getSize().x / 2, m_view->getCenter().y - m_view->getSize().y / 2 };
+                const auto camSize = m_view->getSize();
+
+                const auto sizeRatio = camSize.componentWiseMul(m_bgSize);
+                const auto parallaxCamPos = camPos.componentWiseMul(sizeRatio);
+
+                m_bg->SetScreenCoords({ parallaxCamPos, camSize });
                 m_bg->setPosition(camPos);
             }
         }
@@ -52,6 +60,8 @@ namespace shak
 
     private:
         std::shared_ptr<Background> m_bg;
+        /// @brief Inverse of the background size. Used to compute map size ratio for parallax effect.
+        sf::Vector2f m_bgSize;
         std::shared_ptr<sf::View> m_view;
     };
 }
